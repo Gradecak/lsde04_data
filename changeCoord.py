5152bfbb55df5ba6b789b1b01f240663c564480e
Marijan Gradecak <gradecam@tcd.ie>
2018-10-28 22:05:01 +0000

    a

--- changeCoord.py
+++ changeCoord.py
@@ -1,10 +1,12 @@
 import geopandas as gpd
 import sys
+import os
 
-fname = sys.argv[1]
-outfile = sys.argv[2]
-df = gpd.read_file(fname)
-print(df.crs)
-x = df.to_crs(epsg=4326)
-print(x.crs)
-x.to_file(filename=outfile, driver="GeoJSON")
+
+for filename in os.listdir("./"):
+  df = gpd.read_file(filename)
+  print(df.crs)
+  x = df.to_crs(epsg=4326)
+  print(x.crs)
+  outfile = "p%s" % filename
+  x.to_file(filename=outfile, driver="GeoJSON")
